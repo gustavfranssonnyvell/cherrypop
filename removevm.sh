2b81fe5dd1b6228b2f6eee6607b73556eb237b67
Gustav <gustav@gustav-P15xEMx.nyvell.se>
2015-09-16 06:21:55 +0200

    TODO: add the option to specify the list of hosts

--- removevm.sh
+++ removevm.sh
@@ -1,3 +1,3 @@
 VMNAME=$1
-for i in 192.168.1.68 192.168.1.79; do ssh $i "virsh destroy $VMNAME" & ssh $i "virsh undefine $VMNAME" & done
-for i in 192.168.1.68 192.168.1.79; do ssh $i "virsh undefine $VMNAME" & done
+for i in 192.168.1.69 192.168.1.79; do ssh $i "virsh destroy $VMNAME" & ssh $i "virsh undefine $VMNAME" & done
+for i in 192.168.1.69 192.168.1.79; do ssh $i "virsh undefine $VMNAME" & done
