38a5d2521037e9a8fd9e1d22e11b8fbf7189a49f
Gustav <gustav@gustav-P15xEMx.nyvell.se>
2015-09-17 22:30:36 +0200

    Deb skeleton and script for generating .deb.

---
+++ makedeb.sh
@@ -0,0 +1,11 @@
+VER=cherrypop_0.1beta-1
+compileok() {
+	DEST=$VER
+	for BIN in discoveryd distributevms vmrunnerd runner.sh; do
+	cp $BIN $DEST/usr/sbin/
+	done
+	cp init.d/cherrypop $DEST/etc/init.d/
+	dpkg-deb --build $DEST
+	
+}
+./compile.sh && compileok
