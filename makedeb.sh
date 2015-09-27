ced02dd8e45f46b6f981cd15bddf09cec8db70e1
Gustav <gustav@gustav-P15xEMx.nyvell.se>
2015-09-27 21:52:02 +0200

    Add deletedom.

--- makedeb.sh
+++ makedeb.sh
@@ -1,7 +1,7 @@
 VER=cherrypop_0.2beta-1
 compileok() {
 	DEST=$VER
-	for BIN in discoveryd cherrypop-replicated vmrunnerd runner.sh; do
+	for BIN in discoveryd cherrypop-replicated vmrunnerd runner.sh deletedom; do
 	cp $BIN $DEST/usr/sbin/
 	done
 	cp init.d/cherrypop $DEST/etc/init.d/
